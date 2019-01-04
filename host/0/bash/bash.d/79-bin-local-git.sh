4a138a9765d2a8b2a97a0dc45cd15d498c21bfcf
nobody <nobody@example.com>
2019-01-04 11:01:24 -0600

    added all possible local git repos to 79-bin-local-git.sh

--- host/0/bash/bash.d/79-bin-local-git.sh
+++ host/0/bash/bash.d/79-bin-local-git.sh
@@ -1,25 +1,33 @@
 ## local user/host git bin paths
 
 for basedir in \
-  /usr/local/git/config-public \
-  /usr/local/git/config-private \
-  ~/tmp/git/bbgit2/config-public \
-  ~/tmp/git/bbgit2/config-private \
+  /usr/local/git/ \
+  ~/tmp/git/bbgit2 \
   ; do
-
-  for subdir in \
-    host/0 \
-    host/`hostname` \
-    host/`hostname -a` \
-    user/0 \
-    user/${USER} \
+  
+  for repo in \
+    config-public \
+    config-private \
+    bin-public \
+    bin-private \
     ; do
-
-    for d in ${basedir}/${subdir}/bin ; do
-      [ -d "$d" ] || continue
-      export PATH="${d}:${PATH}"
+    
+    for subdir in \
+      host/0 \
+      host/`hostname` \
+      host/`hostname -a` \
+      user/0 \
+      user/${USER} \
+      ; do
+      
+      for d in ${basedir}/${repo}/${subdir}/bin ; do
+        [ -d "$d" ] || continue
+echo $d
+#        export PATH="${d}:${PATH}"
+      done
+    
     done
-
+  
   done
 
 done
